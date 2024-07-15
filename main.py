4729e6dc6daa51b9d74777393550e45a4f8bbfe8
kannbo <113285773+kannbo@users.noreply.github.com>
2024-07-15 14:47:02 +0900

    Update main.py

--- main.py
+++ main.py
@@ -49,10 +49,51 @@ def comment():
             st.write("成功")
         except Exception as e:
             st.write(f"失敗{e}")
+def getplo(aa):
+    return scratch3.Session(st.session_state["account"][aa]["password"],username=st.session_state["account"][aa]["name"])
 def studio():
-    pass
+    lists=["35441603","32635350","32203566","1435670","31772464","4955848"]
+    st.header('スタジオ')
+    key=st.text_input("キー")
+    studioans=st.text_input("スタジオのID")
+    ploject=st.text_input("プロジェクトのID")
+    if st.button("フォロー!"):
+        aaaa=getplo(key)
+        try:
+            studioa=aaaa.connect_studio(studioans)
+            studioa.follow()
+            st.write("成功")
+        except Exception as e:
+            st.write(f"失敗{e}")
+    if st.button("フォロー解除"):
+        try:
+            studioa=getplo(key).connect_studio(studioans)
+            studioa.unfollow()
+            st.write("成功")
+        except Exception as e:
+            st.write(f"失敗{e}")
+    if st.button("追加!"):
+        try:
+            studioa=getplo(key).connect_studio(studioans)
+            studioa.remove_project(ploject)
+            studioa.add_project(ploject)
+            st.write("成功")
+        except Exception as e:
+            st.write(f"失敗{e}")
+    if st.button("何でもに投稿"):
+        try:
+            aaaa=getplo(key)
+            for i in lists:
+                studioa=aaaa.connect_studio(i)
+                studioa.remove_project(ploject)
+                studioa.add_project(ploject)
+                st.write("成功")
+        except Exception as e:
+            st.write(f"失敗{e}")
+
 app = MultiApp()
 app.add_app("キー生成", page_main)
 app.add_app("編集", page_edit)
 app.add_app("コメント投稿", comment)
+app.add_app("スタジオチート", studio)
 app.run()
